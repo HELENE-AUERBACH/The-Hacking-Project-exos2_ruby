4f0fb8e658caa99cf0999bf83adefecde47fd145
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-04-13 12:43:19 +0200

    Ajout des fichiers des exercices en Ruby

---
+++ exo_12.rb
@@ -0,0 +1,8 @@
+print "Quelle est ton année de naissance? : "
+birth_year = gets.chomp.to_i
+this_year = 2021
+year = birth_year
+while year <= this_year do
+  puts "Il y a #{this_year - year} an#{this_year - year <= 1 ? "" : "s"}, tu #{year == this_year ? "as" : "avais"} #{this_year - year == year - birth_year ? "la moitié de l'âge que tu as aujourd'hui" : "#{year - birth_year} an#{year > birth_year + 1 ? "s" : ""}"}."
+  year = year + 1
+end
